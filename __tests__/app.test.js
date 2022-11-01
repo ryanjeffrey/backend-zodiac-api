fecad0c9bc1cc064ed96b6a63027645f2f3442ac
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-11-01 15:43:50 -0500

    Add test for zodiac route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,14 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('example test - you should probably update me', () => {
-  it('home route should return hello world', async () => {
-    const resp = await request(app).get('/');
-    expect(resp.text).toEqual('hello world!');
+const { zodiac } = require('../lib/zodiac-data');
+
+describe('zodiac routes', () => {
+  it('/zodiac should return a list of zodiac signs', async () => {
+    const res = await request(app).get('/zodiac');
+    const expected = zodiac.map((sign) => {
+      return { id: sign.id, name: sign.name };
+    });
+    expect(res.body).toEqual(expected);
   });
 });
