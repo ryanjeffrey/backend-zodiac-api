ec257699e19df34257c6e3d1dc8a57bc53cd3498
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-11-01 19:20:19 -0500

    Add test for fetching horoscope data

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -23,3 +23,16 @@ describe('zodiac routes', () => {
     expect(res.body).toEqual(libra);
   });
 });
+
+describe('horoscopes routes', () => {
+  it('/horoscopes/:sign should return a horoscope', async () => {
+    const res = await request(app).get('/horoscopes/aquarius');
+    const aquarius = {
+      sign: 'aquarius',
+      date: '2022-11-01',
+      horoscope:
+        'You may have a hard time appreciating the little things this morning, dear Aquarius, as the moon forms a harsh square with the nodes of fate. Certain lifestyle luxuries, personal success, and status may not taste as sweet as you\'d hoped, causing you to close off emotionally. Don\'t be hard on yourself if you\'re feeling disenchanted with the world, but try to reach for that which brings you joy. The vibe will be charged within your domestic affairs when Luna squares off with Uranus this afternoon, so you\'ll want to be on guard for finicky appliances or temperamental roommates.',
+    };
+    expect(res.body).toEqual(aquarius);
+  });
+});
